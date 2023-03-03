20519bcd808c0f027d52192fc2b63a01bad8ea49
S1r0ke <118212411+S1r0ke@users.noreply.github.com>
2023-03-03 13:25:57 +0100

    Delete IST-main-master directory

--- IST-main-master/INSTALLER.ps1
+++
@@ -1,7 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-irm get.scoop.sh | iex
-scoop bucket add extras
-scoop install python
-pip install pillow
-pip install numpy
-pip install tkinter
--- IST-main-master/README.md
+++
@@ -1,2 +0,0 @@
-# IST-main 💀
-Czech program to help with engineering work. I know that my code is messy. Idc tho 💀
